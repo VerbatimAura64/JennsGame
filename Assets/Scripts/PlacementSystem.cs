7831e040c4a6d49c9ae234e1ba6d4cdfe870fa22
William Alonzo <balfondi@gmail.com>
2026-03-12 11:50:45 -0700

    Desktop work that needs to be merged

---
+++ Assets/Scripts/PlacementSystem.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlacementSystem : MonoBehaviour
+{
+    public GameObject mouseIndicator, cellIndicator, bldPrefab;
+    public GridTestInput inputManager;
+    public Grid grid;
+
+    private void Update()
+    {
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int cellPosition = grid.WorldToCell(mousePosition);
+        mouseIndicator.transform.position = mousePosition;
+        cellIndicator.transform.position = grid.CellToWorld(cellPosition);
+
+        if (inputManager.GetPlacementInput())
+        {
+            Instantiate(bldPrefab, cellIndicator.transform.position, Quaternion.identity);
+        }
+    }
+}
